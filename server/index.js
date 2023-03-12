e2ba6e88f6715dd1fa253bd2d7003899bc533d70
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2023-03-12 15:39:25 +0000

    check server

--- server/index.js
+++ server/index.js
@@ -11,14 +11,17 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send("Hello from server");
+})
 
-if (process.env.NODE_ENV === "production") {
-  console.log("In production stage");
-  app.use(express.static(path.resolve(__dirname, '../', "client", "build")))
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '../', "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   console.log("In production stage");
+//   app.use(express.static(path.resolve(__dirname, '../', "client", "build")))
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, '../', "client", "build", "index.html"));
+//   });
+// }
 app.use("/api", require("./routes/route"));
 
 app.listen(port, () => {
