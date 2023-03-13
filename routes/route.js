83737f78c8c908fb1bc2d79373dbe62599514a53
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2023-03-13 02:25:43 +0000

    may be error here

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const router = express.Router();
+const router = express();
 const { Fileschema } = require("./../models/file");
 const upload = multer({ dest: "./../uploads" });
 var cloudinary = require("cloudinary").v2;
