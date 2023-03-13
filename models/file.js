de7e93fe3a82f418443bd314672af97c78994e8c
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2023-03-13 01:48:30 +0000

    update file structure

--- models/file.js
+++ models/file.js
@@ -1,34 +1,34 @@
-const db = require('../db/db');
+const db = require('./../db/db');
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
 const validator = require("validator");
 
 const fileschema = new mongoose.Schema({
-    fileName:{
+    fileName: {
         type: String,
         required: true,
     },
-    urlLink:{
-        type:String,
-        required:true,
-    },
-    format : {
-        type:String,
-        required:true
-    },
-    fileSize:{
+    urlLink: {
         type: String,
-        required:true,
+        required: true,
     },
-    sender:{
-        type:String,
+    format: {
+        type: String,
+        required: true
     },
-    receiver:{
-        type:String,
+    fileSize: {
+        type: String,
+        required: true,
+    },
+    sender: {
+        type: String,
+    },
+    receiver: {
+        type: String,
     }
 })
 
-const Fileschema=mongoose.model('fileschema',fileschema);
-module.exports={
- Fileschema
+const Fileschema = mongoose.model('fileschema', fileschema);
+module.exports = {
+    Fileschema
 }
